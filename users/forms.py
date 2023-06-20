63a1db571c15e00ab8e709c988c6de064cf58319
Karume-lab <danielkarume@outlook.com>
2023-06-20 17:41:39 +0300

    login and sign up configuration

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import User
 
 
 class SignupForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField( max_length=100, required=True)
     mobile_number = forms.IntegerField(required=True, widget=forms.TextInput)
+    email = forms.CharField( max_length=100, required=True)
+    username = forms.CharField(max_length=100, required=True)
+    password1 = forms.PasswordInput()
+    password2 = forms.PasswordInput()
 
     class Meta:
         model = User
