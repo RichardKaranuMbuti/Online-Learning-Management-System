4dd998b5059487a97bff306f62f4d12261e7df60
Javan <javan.o.otieno@gmail.com>
2023-06-22 16:52:11 +0300

    Fix: Added a __str__ function to the Department and Unit models

--- school/models.py
+++ school/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 class Department(models.Model):
-    code = models.CharField('Department Code', max_length=20, primary_key=True)
     name = models.CharField('Department Name', max_length=100)
+    code = models.CharField('Department Code', max_length=20, primary_key=True)
     description = models.TextField('Department Description')
+
+    def __str__(self) -> str:
+        return self.name
 
 class Unit(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     code = models.CharField('Unit Code', max_length=20, primary_key=True)
     name = models.CharField('Unit Name', max_length=100)
     description = models.TextField('Unit Description')
+
+    def __str__(self) -> str:
+        return self.name
